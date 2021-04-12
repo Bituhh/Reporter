56d745c2180ca3471900934aae3e7ac2ea08a7a3
Victor Oliveira <victor@oliveira.org.uk>
2021-04-12 18:21:55 +0100

    adding reports section

---
+++ src/app/reports/reports.module.ts
@@ -0,0 +1,13 @@
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {ReportsComponent} from './reports.component';
+
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+    CommonModule,
+  ],
+})
+export class ReportsModule {
+}
